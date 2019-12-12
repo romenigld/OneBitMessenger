0269c5ce6c5ab751868020d2899344724de3c584
romenigld <romenigld@gmail.com>
2019-12-12 20:23:01 +0100

    Aula 2: Novo Contato; Remover Contato;

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,37 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = current_user.contacts
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    user = User.find_by(email: contact_params[:record][:email])
+    @contact = current_user.contacts.build(record: user)
+    if @contact.save
+      flash[:notice] = 'Contact successfully created'
+      redirect_to contacts_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @contact = current_user.contacts.find(params[:id])
+    if @contact.destroy
+      flash[:notice] = "Contact successfully removed"
+    else
+      flash[:alert] = "An error ocurred when trying to remove this contact"
+    end
+
+    redirect_to contacts_path
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(record: :email)
+  end
+end
