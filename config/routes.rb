643dfc0355565f6b9a517716cb6b98ebc07c2504
romenigld <romenigld@gmail.com>
2019-12-14 20:30:16 +0100

    Aula 3: Exibir um chat; Enviar mensagem

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   resources :contacts, only: %i(index new create destroy)
+  resources :chats, only: %i(index show) do
+    resources :messages, only: :create, defaults: { format: :js }
+  end
 end
